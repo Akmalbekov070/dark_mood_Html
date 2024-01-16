ccffbb6634cef9c052d22549d5ca656da660b976
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-01-16 22:20:55 +0300

    for loop

---
+++ java/java.js
@@ -0,0 +1,102 @@
+// const person = {
+// 	firstName: 'John',
+// 	lastName: 'Doe',
+// 	fullName: function () {
+// 		console.log(this.firstName + ' ' + this.lastName);
+// 	},
+// };
+
+// person.fullName();
+// let x = 5;
+// let y = 10;
+// let z = x + y;
+// alert(z);
+
+// let day;
+// switch (new Date(3).getDay()) {
+// 	case 0:
+// 		day0 = 'Sunday';
+// 		break;
+// 	case 1:
+// 		day1 = 'Monday';
+// 		break;
+// 	case 2:
+// 		day2 = 'Tuesday';
+// 		break;
+// 	case 3:
+// 		day3 = 'Wednesday';
+// 		break;
+// 	case 4:
+// 		day4 = 'Thursday';
+// 		break;
+// 	case 5:
+// 		day5 = 'Friday';
+// 		break;
+// 	case 6:
+// 		day6 = 'Saturday';
+// }
+// console.log(day);
+// let checkValue;
+
+// function checkValue(a, b) {
+// 	if (a === 1)
+// 		if (b === 2) console.log('a is 1 and b is 2');
+// 		else console.log('a is not 1');
+// }
+
+// let day = 0;
+// switch (day) {
+// 	case 0:
+// 		console.log('Sunday');
+// 		break;
+// 	case 1:
+// 		'Monday';
+// 		break;
+// 	case 2:
+// 		'Tuesday';
+// 		break;
+// 	case 3:
+// 		'Wednesday';
+// 		break;
+// 	case 4:
+// 		'Thursday';
+// 		break;
+// 	case 5:
+// 		'Friday';
+// 		break;
+// 	case 6:
+// 		'Saturday';
+// }
+// console.log(day);
+
+// const b = new Boolean(false);
+// if (b) {
+// 	console.log('b is truthy'); // "b is truthy"
+// }
+// let input = prompt(`Say "yes" or "no"`);
+// if (input === 'yes') {
+// 	console.log('Oh yeah!');
+// } else {
+// 	console.log('Oh no!');
+// }
+//  let input = prompt(`Say "yes" or "no"`);
+//  if (input === 'yes') {
+// 		console.log('Oh yeah!');
+//  } else if (input === 'no') {
+// 		console.log('Oh no!');
+//  } else {
+// 		console.log('Oh, you said something else...');
+//  }
+
+//  let age = prompt('What is your age?');
+//  let ageAsNumber = Number(age);
+//  if (ageAsNumber < 10) {
+// 		console.log('You are too young to play this game');
+//  } else {
+// 		console.log('Welcome to role playing game!');
+//  }
+
+for (let i = 0; i < 100; i++) {
+	console.log('Umr botga Sevaman ❤️');
+	('<br>');
+}
