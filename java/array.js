a04fb05a3b0651b3f7bad98a0b889895ab01de2a
Akmalbekov070 <akmalbekov2022@gmail.com>
2024-01-23 12:20:23 +0300

    acommit method

--- java/array.js
+++ java/array.js
@@ -1,3 +1,21 @@
 const array = ['olma', 'nok', 'xurmo', 'gilos'];
+const arrayes = ['olma', 'yuq', 'xurmo', 'gilos'];
+const array3 = array.concat(arrayes);
+// console.log(arrayes);
 
-console.log(array);
+const array1 = [3, 66, 78];
+const array2 = ['d', 'e', 'f'];
+// const array4 = array1.keys(element => element > 1);
+const array4 = array1.flatMap(num => (num === 2 ? [2, 2] : 190));
+
+console.log(array4);
+
+//find bir xonali ikki xonali sonlar va boshqalarni olib beradi
+//push bu qushadi
+//shift boshidan arrayni olib tashlaydi
+//unshift bu arrayga suz qushadi
+//concat bu ikkala arrayni bir biriga qushadi
+////indexOff bu nechi indexda turganini kursatib beradi
+//keys bu massivda qullaniladi faqat klujini olib beradi
+//flatMap bu tenglashtiradi
+//sort bu orqada turgan arraylarni oldinga joylashtiradi
